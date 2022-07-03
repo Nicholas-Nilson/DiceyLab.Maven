54afa2289f875e5001a2b349f4d59493b5bb252c
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-03 15:42:48 -0400

    tossAndSum written

---
+++ src/Test/java/DiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DiceTest {
+
+    @Test  //this may be completely unnecessary.
+    public void testTossAndSum() {
+        // Given
+        Integer minRoll = 1;
+        Integer maxRoll = 6;
+        // When
+        Dice dice = new Dice(1);
+        // Then
+        boolean actual;
+        Integer actualRoll = dice.tossAndSum(1);
+        if (minRoll <= actualRoll && actualRoll <= maxRoll) {
+            actual = true;
+        } else { actual = false;}
+        Assertions.assertTrue(actual);
+
+    }
+}
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,17 @@
 public class Dice {
 
+    Integer sides = 6;
+    Integer numberOfDice;
+
+    public Dice(Integer numberOfDies) {
+        this.numberOfDice = numberOfDies;
+    }
+    public Integer tossAndSum (Integer numberOfDies) {
+        Integer result = 0;
+        for (int i = 0; i < numberOfDies; i++) {
+            result = (int)(sides * Math.random()) + 1;
+        }
+        return result;
+    }
 
 }
