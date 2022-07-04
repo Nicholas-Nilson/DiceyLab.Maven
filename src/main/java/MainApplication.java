6cd3f7d53612dcbe9683cc43d3660f1beded850c
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-04 15:08:36 -0400

    printresults written & working

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class MainApplication {
 
     public static void main(String args[]) {
-        Simulation sim1 = new Simulation(2, 10000);
+        Simulation sim1 = new Simulation(2, 1000000);
         sim1.runSimulation();
 //        sim1.getPercentages();
 //        System.out.println(Arrays.toString(sim1.percentResults));
