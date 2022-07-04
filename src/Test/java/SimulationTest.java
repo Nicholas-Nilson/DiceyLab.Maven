77ca6163bf15fa713741ffeace4b6124c919e73a
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-04 16:13:39 -0400

    commit before messing with print result to return a string

--- src/Test/java/SimulationTest.java
+++ src/Test/java/SimulationTest.java
@@ -52,25 +52,39 @@ class SimulationTest {
     }
 
     @Test
-    public void testGetPercentages2() {
+    public void testPrintResults() {
         //Given
-        Double[] expected = {.03, .06, .08, .11, .14, .17, .14, .11, .08, .06, .03};
+        String expected = " ***\n"+
+                            "Simulation of 2 dice tossed for 1000000 times.\n"+
+                            "***\n\n"+
+
+        "2:     27848: 0.03 ***\n"+
+        "3:     55286: 0.06 ******\n"+
+        "4:     83432: 0.08 ********\n"+
+        "5:    110740: 0.11 ***********\n"+
+        "6:    138915: 0.14 **************\n"+
+        "7:    166833: 0.17 *****************\n"+
+        "8:    139399: 0.14 **************\n"+
+        "9:    110417: 0.11 ***********\n"+
+        "10:     82971: 0.08 ********\n"+
+        "11:     56104: 0.06 ******\n"+
+        "12:     28055: 0.03 ***\n";
         //When
-        Simulation testSim = new Simulation(2, 100);
-        testSim.rolls.results[0] = 3;
-        testSim.rolls.results[1] = 6;
-        testSim.rolls.results[2] = 8;
-        testSim.rolls.results[3] = 11;
-        testSim.rolls.results[4] = 14;
-        testSim.rolls.results[5] = 17;
-        testSim.rolls.results[6] = 14;
-        testSim.rolls.results[7] = 11;
-        testSim.rolls.results[8] = 8;
-        testSim.rolls.results[9] = 6;
-        testSim.rolls.results[10] = 3;
-        testSim.getPercentages();
+        Simulation testSim = new Simulation(2, 100000);
+        testSim.rolls.results[0] = 27848;
+        testSim.rolls.results[1] = 55286;
+        testSim.rolls.results[2] = 83432;
+        testSim.rolls.results[3] = 110740;
+        testSim.rolls.results[4] = 138915;
+        testSim.rolls.results[5] = 166833;
+        testSim.rolls.results[6] = 139399;
+        testSim.rolls.results[7] = 110417;
+        testSim.rolls.results[8] = 82971;
+        testSim.rolls.results[9] = 56104;
+        testSim.rolls.results[10] = 28055;
+        testSim.printResults();
         //Then
-        Double[] actual = testSim.percentResults;
+        String actual = testSim.printResults().toString();
         Assertions.assertArrayEquals(expected, actual);
     }
 
